1cc8c162208d01919e9547b381f06fd2f8fe1b9c
blockchainer2022 <91312709+blockchainer2022@users.noreply.github.com>
2021-12-18 19:03:11 +0530

    update responsive

--- src/sections/Banner.js
+++ src/sections/Banner.js
@@ -42,8 +42,8 @@ const Banner = () => {
   return (
     <section className="banner">
       <div className="container py-20 pb-48">
-        <div className="grid lg:grid-cols-2 gap-8 items-center">
-          <div className="mt-28 lg:mt-0" data-aos="fade-right">
+        <div className="grid lg:grid-cols-2 gap-20 lg:gap-8 items-center">
+          <div className="mt-20 lg:mt-0" data-aos="fade-right">
             <h1 className="text-4xl md:text-5xl font-bold text-center lg:text-left">
               <span className="text-white mb-2.5 inline-block">
                 Blast off With
@@ -54,31 +54,32 @@ const Banner = () => {
                 <img src={Rocket} alt="" className="ml-4" />
               </span>
             </h1>
-            <p className="text-gray-400 mt-5">
+            <p className="text-center lg:text-left text-gray-400 mt-5 max-w-3xl mx-auto w-full">
               With our large marketing team, rewarding tokenomics, and ambitious
               roadmap, we're confident Lunar Doge will soar farther than any
               other Doge coin.
             </p>
-            <ul className="grid grid-flow-col gap-4 justify-start items-center  mx-auto lg:mx-0  w-full text-white mt-10  flex-wrap">
-              {iconList.map((v, i) => (
-                <li key={i}>
-                  <a
-                    href={v.link}
-                    className="w-16 h-16 border border-white bg-black  grid place-content-center text-2xl hover:bg-primary hover:text-black transition-all duration-300 rounded-sm"
-                  >
-                    {v.icon}
-                  </a>
-                </li>
-              ))}
-              <li>
-                <button className="banner-button flex items-center py-2 px-7 text-white mx-auto capitalize">
-                  <img src={Lunar2} alt="" className="  w-14" />{" "}
-                  <span className="font-bold ml-4 inline-block text-lg">
-                    LunarExchange
-                  </span>
-                </button>
-              </li>
-            </ul>
+            <div className="flex  flex-col lg:flex-row items-center lg:justify-start justify-center mt-8">
+              <ul className="grid grid-flow-col gap-4 justify-center lg:justify-start items-center  text-white  flex-wrap">
+                {iconList.map((v, i) => (
+                  <li key={i}>
+                    <a
+                      href={v.link}
+                      className="w-16 h-16 border border-white bg-black  grid place-content-center text-2xl hover:bg-primary hover:text-black transition-all duration-300 rounded-sm"
+                    >
+                      {v.icon}
+                    </a>
+                  </li>
+                ))}
+                <li></li>
+              </ul>
+              <button className="banner-button mt-6 lg:mt-0 flex items-center py-2 px-7 text-white capitalize">
+                <img src={Lunar2} alt="" className="  w-14" />{" "}
+                <span className="font-bold ml-4 inline-block text-lg">
+                  LunarExchange
+                </span>
+              </button>
+            </div>
           </div>
           <div className="text-center " data-aos="fade-left">
             <img src={Doge} alt="" className="mx-auto" />
